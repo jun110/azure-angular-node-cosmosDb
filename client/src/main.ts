2448ef4cbc84eaa3a8574c77a866a3c2addf37b7
jun kitayama <junkitayama@microsoft.com>
2021-10-06 16:27:43 +0900

    Commit

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const appInsights = new ApplicationInsights({ config: {
+  instrumentationKey: '06cb6361-b98d-4003-bf0c-8e5b7f80c74b',
+  distributedTracingMode: 2
+  /* ...Other Configuration Options... */
+} });
+appInsights.loadAppInsights();
+appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
+
+
 if (environment.production) {
   enableProdMode();
 }
